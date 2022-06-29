f654d26f29c854bbfb0a2881e3702bc27a4fe78a
Ikilzig <palvarezimaz@gmail.com>
2022-06-29 17:44:48 +1000

    client-server comm established

--- README.md
+++ README.md
@@ -5,3 +5,21 @@
 A proof of concept real-time message app using React as a Front end, Express and PostgreSQL as a Back end, servead by Socket.io as a websocket implementation
 
 This is served by the ChatApp Server [here](https://github.com/palvarezimaz/chatapp-server).
+
+## Development plan
+
+1. Implement WebSockets (via Socket.io) to enable messaging between clients
+2. Build chatrooms. Users can access them freely (later implementation - with a password)
+3. Implement direct messages between users
+
+4. Login and registration
+
+- MVP
+
+5. Extra develompents.
+   i. Option to log or ignore messages
+   ii. Export conversations
+   iii. Day/night mode
+   iv. Login options
+   v. Image sharing
+   vi. Gifs
--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import io from 'socket.io-client';
 
-function App() {
+const App = () => {
+  const [time, setTime] = React.useState('fetching');
+  React.useEffect(() => {
+    const socket = io('http://localhost:3002');
+    socket.on('connect', () => console.log(socket.id));
+    socket.on('connect_error', () => {
+      setTimeout(() => socket.connection(), 3002);
+    });
+
+    socket.on('time', (data) => setTime(data));
+    socket.on('disconnect', () => setTime('server disconnected'));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{time}</p>
+
+      {/* <span>State: {this.state.connected ? 'Connected' : 'Disconnected'}</span>
+      <span>Current transport: {this.state.currentTransport}</span>
+      <span>Last message: {this.state.lastMessage}</span> */}
     </div>
   );
-}
+};
 
 export default App;
+//////////////
+// export default class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       connected: socket.connected,
+//       currentTransport: socket.connected
+//         ? socket.io.engine.transport.name
+//         : '-',
+//       lastMessage: '',
+//     };
+//   }
+
+//   componentDidMount() {
+//     socket.on('connect', () => this.onConnectionStateUpdate());
+//     socket.on('disconnect', () => this.onConnectionStateUpdate());
+//     socket.on('message', (content) => this.onMessage(content));
+//   }
+
+//   componentWillUnmount() {
+//     socket.off('connect');
+//     socket.off('disconnect');
+//     socket.off('message');
+//   }
+
+//   onConnectionStateUpdate() {
+//     this.setState({
+//       connected: socket.connected,
+//       currentTransport: socket.connected
+//         ? socket.io.engine.transport.name
+//         : '-',
+//     });
+//     if (socket.connected) {
+//       socket.io.engine.on('upgrade', () => this.onUpgrade());
+//     } else {
+//       socket.io.engine.off('upgrade');
+//     }
+//   }
+
+//   onMessage(content) {
+//     this.setState({
+//       lastMessage: content,
+//     });
+//   }
+
+//   onUpgrade() {
+//     this.setState({
+//       currentTransport: socket.io.engine.transport.name,
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <span>
+//           State: {this.state.connected ? 'Connected' : 'Disconnected'}
+//         </span>
+//         <span>Current transport: {this.state.currentTransport}</span>
+//         <span>Last message: {this.state.lastMessage}</span>
+//       </div>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
