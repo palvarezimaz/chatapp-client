205e36f14f8fb802c43de6db59b404c8bed122e2
Pablo <palvarezimaz@gmail.com>
2025-03-21 17:21:35 -0300

    cors

--- src/socket.js
+++ src/socket.js
@@ -4,7 +4,12 @@ let URL = 'http://localhost:3002';
 if (process.env.NODE_ENV === 'production') {
   URL = 'https://github.com/palvarezimaz/chatapp-server';
 }
-const socket = io(URL, { autoConnect: false });
+const socket = io(URL, { 
+  withCredentials: true, 
+  extraHeaders: {
+    "my-custom-header": "ChatApp"
+  },
+  autoConnect: false });
 
 ////// UNCOMMENT FOR DEVELOPMENT - LOGS ANY SOCKET EVENT
 // socket.onAny((event, ...args) => {
